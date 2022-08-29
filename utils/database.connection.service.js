e0a95df25d1130d3f35d7e6b68772a22b2fe3cf1
phillip-hlapa <philliphlapa2015@gmail.com>
2022-08-29 06:12:30 +0200

    cloud db

--- utils/database.connection.service.js
+++ utils/database.connection.service.js
@@ -2,11 +2,11 @@ var APP_CONSTANTS = require('./constants');
 var mongoose = require('mongoose')
 
 module.exports.connect = () => {
-    // const db_url = 'mongodb+srv://' + process.env.MONGO_DB_USER + ':' + process.env.MONGO_DB_PASS + '@cluster0.xbnnw.mongodb.net/' + process.env.MONGO_DB_DATABASE_YNIT_TASK_TRACKER + '?retryWrites=true&w=majority';
-    const db_url_local = process.env.MONGO_DB_LOCAL_INSTANCE + '/YNIT_TASK_TRACKER_DB'
+    const db_url = 'mongodb+srv://' + process.env.MONGO_DB_USER + ':' + process.env.MONGO_DB_PASS + '@cluster0.xbnnw.mongodb.net/' + process.env.MONGO_DB_DATABASE_YNIT_TASK_TRACKER + '?retryWrites=true&w=majority';
+    //const db_url_local = process.env.MONGO_DB_LOCAL_INSTANCE + '/YNIT_TASK_TRACKER_DB'
     try {
-        console.log(db_url_local)
-        mongoose.connect(db_url_local, {useNewUrlParser: true,  useUnifiedTopology: true}, (error, result) => {
+        console.log(db_url)
+        mongoose.connect(db_url, {useNewUrlParser: true,  useUnifiedTopology: true}, (error, result) => {
             console.log("connected to DB")
         });
     } catch (error) {
