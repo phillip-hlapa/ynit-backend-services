3e061e8e61a7ff1d3d2a42a34b9bbe3cb64ec626
phillip-hlapa <philliphlapa2015@gmail.com>
2022-08-29 13:46:31 +0200

    debugging

--- utils/database.connection.service.js
+++ utils/database.connection.service.js
@@ -2,7 +2,7 @@ var constant = require('./constants');
 var mongoose = require('mongoose')
 
 module.exports.connect = () => {
-    const db_url = 'mongodb+srv://juspotatoes:juspotatoesPassword@cluster0.xbnnw.mongodb.net/' + constant.MONGO_DB_DATABASE_YNIT_TASK_TRACKER + '?retryWrites=true&w=majority';
+    const db_url = 'mongodb+srv://' + constant.MONGO_DB_USER + ':' + constant.MONGO_DB_PASS + '@cluster0.xbnnw.mongodb.net/' + constant.MONGO_DB_DATABASE_YNIT_TASK_TRACKER + '?retryWrites=true&w=majority';
     //const db_url_local = process.env.MONGO_DB_LOCAL_INSTANCE + '/YNIT_TASK_TRACKER_DB'
     try {
         console.log(db_url)
