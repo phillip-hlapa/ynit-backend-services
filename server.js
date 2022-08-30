bb1712514c48f1430decddfcac2c01db3a2f2b6f
phillip-hlapa <philliphlapa2015@gmail.com>
2022-08-30 19:20:05 +0200

    some services

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/api/test', (req, res) => {
     res.send("hello world!");
 })
 
+app.get('/', (req, res) => {
+    res.send("<H2>Welcome to YNIT Master Backend Service! @ <i>" + new Date() + "</i></H2>")
+})
 app.use('/ynitapi', GLOBAL_ROUTER)
 
 app.listen(process.env.PORT, (err, result) => {
