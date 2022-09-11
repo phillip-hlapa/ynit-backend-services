4713c018c6ce9cf5e048d262e9e4dd77e446e24f
phillip-hlapa <philliphlapa2015@gmail.com>
2022-09-11 14:59:42 +0200

    added data tracking

--- models/ynit-my-task-tracker-master-models/request.data.model.js
+++ models/ynit-my-task-tracker-master-models/request.data.model.js
@@ -5,7 +5,7 @@ const DateUtil = require('../../utils/data.util')
 const  RequestData = mongoose.Schema({
     path: { type: String, require: true },
     description: { type: String, require: true },
-    date: { type: Date, require: false, default: Date.parse(DateUtil.getDate().toString()) },
+    date: { type: Date, value: Date.parse(DateUtil.getDate().toString()) },
 })
 
 module.exports = mongoose.model('RequestData', RequestData);
