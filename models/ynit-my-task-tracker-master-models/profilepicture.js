ab98f7b172928dfb6113b9600646e1c92c84c0b7
phillip-hlapa <36256993+phillip-hlapa@users.noreply.github.com>
2022-09-12 05:39:04 +0200

    Added user ID

--- models/ynit-my-task-tracker-master-models/profilepicture.js
+++ models/ynit-my-task-tracker-master-models/profilepicture.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const  ProfilePic = mongoose.Schema({
+    userId: { type: String, require: false },
     data: { type: String, require: true },
     name: { type: String, require: false, default: "WHO WROTE THIS COMMENT" },
     date: { type: Date, require: false, default: Date.now },
